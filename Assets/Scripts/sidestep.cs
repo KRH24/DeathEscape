ad27c6549150bed70f77911c67fd7434a02c0449
Robert Cole <rjcole@usc.edu>
2025-05-31 11:57:54 -0700

    Update sidestep.cs

--- Assets/Scripts/sidestep.cs
+++ Assets/Scripts/sidestep.cs
@@ -1,9 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class sidestep : MonoBehaviour
 {
+	if(input){
+
+
+}
     // Start is called before the first frame update
     void Start()
     {
