8d81ac0efb1ed4ea7e2e4d2e222e730c0da8d112
Robert Cole <rjcole@usc.edu>
2025-06-01 12:06:18 -0700

    Deleted movement, readded sidestep script, and added a physics material
    The movement script was too buggy so i am going to restart.

---
+++ Assets/Scripts/Sidestep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sidestep : MonoBehaviour
+{
+	
+	public Transform Player;
+	public Rigidbody rb;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if(Input.GetKeyDown(KeyCode.RightArrow)){
+	    	
+		    if(Input.GetKeyDown(KeyCode.RightArrow)){
+		    	
+		    	rb.AddForce( 50f, 0 , 0);
+		       
+		    }	
+	    	
+	    }
+    }
+}
