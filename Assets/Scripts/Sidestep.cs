0278f0e947362f2fd6e88345158f9d5a9bfb0af8
Robert Cole <rjcole@usc.edu>
2025-06-01 19:22:07 -0700

    added scripts

--- Assets/Scripts/Sidestep.cs
+++ Assets/Scripts/Sidestep.cs
@@ -7,23 +7,35 @@ public class Sidestep : MonoBehaviour
 	
 	public Transform Player;
 	public Rigidbody rb;
+	bool hasPressed = false; 
+	int count = 0;
 	
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+	private float lastLapTime = 0f; 
+	private float threshold = 0.3f;
+  
     // Update is called once per frame
     void Update()
     {
-	    if(Input.GetKeyDown(KeyCode.RightArrow)){
+	    if(Input.GetKeyDown(KeyCode.A)){
 	    	
-		    if(Input.GetKeyDown(KeyCode.RightArrow)){
+	    	hasPressed = true;
+	    	//count += 1;
+	    	
+	    	float CurrentTime = Time.time;
+	    	
+	    	if(CurrentTime - lastLapTime <= threshold){
+	    	
+		    rb.AddForce( 50f, 0 , 0, ForceMode.Impulse);
+		    lastLapTime = 0f;
+		    
+		    }
+		    else{
 		    	
-		    	rb.AddForce( 50f, 0 , 0);
+			    lastLapTime = 0f;
+		    	
+		    }
 		       
-		    }	
+		    	
 	    	
 	    }
     }
